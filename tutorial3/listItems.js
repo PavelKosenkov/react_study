bdcea3265154c7adbf2e8d756c884d762ea2b6fe
Pavel Kasiankou <p.kasiankou@tio.nl>
2018-03-04 10:55:33 +0100

    and without state

--- tutorial3/listItems.js
+++ tutorial3/listItems.js
@@ -4,16 +4,14 @@ class ListItems extends React.Component
 {
     constructor(props){
         super(props);
-        this.state = {
-            dataMapped : props.data.map(i=><li key={i}>{i}</li>)
-        }
+        this.mappedData = props.data.map(i=><li key={i}>{i}</li>);
     }
 
     render()
     {
         return (
             <ul>
-                {this.state.dataMapped}
+                {this.mappedData}
             </ul>
         );
     }
