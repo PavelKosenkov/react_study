ae63027dde9c985c0c0c03122321dcdc96422745
Pavel Kasiankou <p.kasiankou@tio.nl>
2018-03-05 17:33:24 +0100

    to be tested still. lifting state.

--- LiftingTheWholeState/index.js
+++ LiftingTheWholeState/index.js
@@ -5,10 +5,31 @@ function BoilingVerdict(props) {
     return <p>The water will not boil</p>
 }
 
-const scaleName={
+const scaleNames={
     c: 'celsius',
     f: 'fahrenheit'
 }
+
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+}
+
+//careful : here we accept convert object
+function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+      return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+  }
+  
+  
 
 class TemperatureUnit extends React.Component {
     constructor(props){
@@ -18,12 +39,13 @@ class TemperatureUnit extends React.Component {
     }
 
     handleChange(e) {
-        this.setState({temperature: e.target.value});
+        this.props.onTemperatureChange(e.target.value);
+        //this.setState({temperature: e.target.value});
     }
 
     render() {
         //obviously this is not part of the return function
-        const temperature = this.state.temperature;
+        const temperature = this.props.temperature;
         const scale = this.props.scale;
 
         //and render an element. Mind the space bar
@@ -39,7 +61,54 @@ class TemperatureUnit extends React.Component {
     }
 }
 
-class Calculator extends React.Component{
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+        this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+
+        this.state = {
+          temperature: '212',
+          scale: 'f'
+        };
+    }
+
+    handleCelsiusChange(temperature) {
+        this.setState({scale: 'c', temperature});
+    }
+
+    handleFahrenheitChange(temperature) {
+    this.setState({scale: 'f', temperature});
+    }
+
+    render() {
+        
+        //a good use of an expression
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
+        //we do not need to use if in an event to determine which temperature convert to which
+        //we know which edit box have supplied the value
+        //so we can just create 2 separate events and call them accordingly.
+        return (
+            <div>
+                <TemperatureInput
+                    scale="c"
+                    temperature={celsius}
+                    onTemperatureChange={this.handleCelsiusChange} />
+                <TemperatureInput
+                    scale="f"
+                    temperature={fahrenheit}
+                    onTemperatureChange={this.handleFahrenheitChange} />
+                <BoilingVerdict
+                celsius={parseFloat(celsius)} />
+            </div>
+        );
+    }
+}
+
+class Calculator_old extends React.Component{
     constructor(props){
         super(props)
         this.state =
