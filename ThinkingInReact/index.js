c5072ec0cd0c1f014f54a4a80d5122be4188c012
PK <p.kasiankou@tio.nl>
2018-03-07 17:02:19 +0100

    Thinking in react start

---
+++ ThinkingInReact/index.js
@@ -0,0 +1,23 @@
+const dataSample = [
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+  ];
+
+  
+
+// FilterableProductTable
+//     SearchBar
+
+//     ProductTable
+//         ProductCategoryRow
+//         ProductRow
+
+
+ReactDOM.render(
+    'Test',
+    document.getElementById("target")
+);
